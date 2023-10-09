8344d961f737e2d5da9824e0ad7729610a2f1b30
meeav <meeav@DESKTOP-GM7C4BE>
2023-10-09 13:47:34 +0300

    Add project files.

---
+++ MatchingGame/Form2.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace MatchingGame
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.highScoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Window;
+            this.easyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.easyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.easyButton.Location = new System.Drawing.Point(571, 50);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(141, 48);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "EASY";
+            this.easyButton.UseVisualStyleBackColor = true;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mediumButton.Location = new System.Drawing.Point(571, 104);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(141, 48);
+            this.mediumButton.TabIndex = 1;
+            this.mediumButton.Text = "MEDIUM";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButton_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hardButton.Location = new System.Drawing.Point(571, 158);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(141, 48);
+            this.hardButton.TabIndex = 2;
+            this.hardButton.Text = "HARD";
+            this.hardButton.UseVisualStyleBackColor = true;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.quitButton.Location = new System.Drawing.Point(571, 266);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(141, 48);
+            this.quitButton.TabIndex = 3;
+            this.quitButton.Text = "QUIT";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // highScoreButton
+            // 
+            this.highScoreButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.highScoreButton.Location = new System.Drawing.Point(571, 212);
+            this.highScoreButton.Name = "highScoreButton";
+            this.highScoreButton.Size = new System.Drawing.Size(141, 48);
+            this.highScoreButton.TabIndex = 4;
+            this.highScoreButton.Text = "Highscore";
+            this.highScoreButton.UseVisualStyleBackColor = true;
+            this.highScoreButton.Click += new System.EventHandler(this.highScoreButton_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.highScoreButton);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.Name = "Form2";
+            this.Text = "Matching Game - Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Button highScoreButton;
+    }
+}
