cfb782440c793473faaa41b83aee0bdb1e53a2f2
Lari Karttunen <p93456@edu.sasky.fi>
2023-10-11 12:23:19 +0300

    Add project files.

---
+++ MatchingGame/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            Form1 newForm = new Form1();
+            newForm.Show();
+            this.Hide();
+        }
+        private void mediumButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void highScoreButton_Click(object sender, EventArgs e)
+        {
+            Form3 newForm = new Form3();
+            newForm.Show();
+            this.Hide();
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close the game?", "Quit Program", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
---
+++ MatchingGame/Form3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MatchingGame.Form1;
+
+namespace MatchingGame
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+            this.Hide();
+
+        }
+
+        public void label1_Click(object sender, EventArgs e)
+        {
+            int time;
+        }
+    }
+}
